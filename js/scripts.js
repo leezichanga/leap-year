cd9b145e54a495e137c8256868491d0f3654c8a9
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-02-20 15:40:36 +0300

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,4 @@
-$(document).ready(function() {
-    $("form#leap-year").submit(function(event) {
-      event.preventDefault();
-      var year = parseInt($("input#year").val());
-      var result = leapYear(year);
-      $("#result").text(result);
-    });
-  });
 
-  var leapYear = function(year) {
-      return false;
-    };
-
-    // jQuery user interface logic here.
-
-    var leapYear = function(year) {
-        if (year % 4 === 0) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      // jQuery user interface logic here.
-
-      var leapYear = function(year) {
-        if (year % 100 === 0) {
-          return false;
-        } else if (year % 4 === 0) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      // jQuery user interface logic here.
-
-      var leapYear = function(year) {
-          if ((year % 4 === 0) && (year % 100 !== 0)) {
-            return true;
-          } else {
-            return false;
-          }
-        };
-
-        // jQuery user interface logic here.
-
-        var leapYear = function(year) {
-            if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-              return true;
-            } else {
-              return false;
-            }
-          };
 
           // jQuery user interface logic here.
 
@@ -81,3 +28,4 @@ $(document).ready(function() {
 
                 $("#result").show();
               });
+            });
